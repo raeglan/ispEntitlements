4c45ef81d7f1ad4cf9162fbc22ba4d4295f8efe3
Rafael Miranda <rafmir@amazon.de>
2019-02-19 13:00:35 +0100

    Everything you need.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
 
-const Alexa = require('ask-sdk-core');
+const Alexa = require('ask-sdk');
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -90,7 +90,103 @@ const ErrorHandler = {
   },
 };
 
-const skillBuilder = Alexa.SkillBuilders.custom();
+const BuyPowerUpIntentHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && request.intent.name === 'BuyPowerUpIntent'
+  },
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+
+    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    const powerUpProduct = sessionAttributes.products.filter(product => product.referenceName === 'PowerUp')[0];
+
+    return handlerInput.responseBuilder
+      .addDirective(
+        {
+          type: "Connections.SendRequest",
+          name: "Buy",
+          payload: {
+            InSkillProduct: {
+              productId: powerUpProduct.productId,
+            }
+          },
+          token: "correlationToken",
+        }
+      )
+      .getResponse();
+  }
+}
+
+const PostBuyConnectionsHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === 'Connections.Response' && (request.name === 'Buy' || request.name === 'Upsell');
+  },
+  handle(handlerInput) {
+
+    // Preparing constants
+    const request = handlerInput.requestEnvelope.request;
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const offeredProductId = request.payload.productId;
+    const offeredProduct = attributes.products.filter(product => product.productId === offeredProductId)[0];
+
+    // Variables for response
+    let speakOutput;
+    let repromptOutput = 'hmm?';
+
+    if (request.status.code === '200') {
+
+      switch(request.payload.purchaseResult) {
+        case 'ACCEPTED':
+          speakOutput = `With ${offeredProduct.name} you can be invisible to no one.`
+          break;
+        case 'DECLINED':
+          speakOutput = `You didn't want to buy ${offeredProduct.name}. What a shame.`;
+          break;
+        case 'ALREADY_PURCHASED':
+          speakOutput = `${offeredProduct.name} grants you invisibility. But it also seems it makes you forget stuff.`;
+          break;
+        default:
+          speakOutput = `Something weird happened. But thanks anyway for trying to give me money.`
+          break
+      }
+
+      return handlerInput.responseBuilder
+          .speak(speakOutput)
+          .reprompt(repromptOutput)
+          .getResponse();
+    } else {
+      // Something failed.
+      console.log(`Connections.Response indicated failure. error: ${handlerInput.requestEnvelope.request.status.message}`);
+
+      return handlerInput.responseBuilder
+        .speak('There was an error handling your purchase request. Please try again or contact us for help.')
+        .getResponse();
+    }ƒ
+  }
+}
+
+const ProductsCheckerInterceptor = {
+  async process(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    if (request.session.new === true) {
+      try {
+        const locale = handlerInput.requestEnvelope.request.locale;
+        const ms = handlerInput.serviceClientFactory.getMonetizationServiceClient();
+        const result = await ms.getInSkillProducts(locale);
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        sessionAttributes.products = result.inSkillProducts;
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+      } catch (error) {
+        console.log(`Error calling InSkillProducts API: ${error}`);
+      }
+    }
+  }
+}
+
+const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
@@ -98,7 +194,11 @@ exports.handler = skillBuilder
     HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
+    SessionEndedRequestHandler,
+    PostBuyConnectionsHandler
+  )
+  .addRequestInterceptors(
+    ProductsCheckerInterceptor
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
